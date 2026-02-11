8b2150340d2164ea040f91a67bf007aaea0b37ec
geetha <geetha.isme.2326@gmail.com>
2026-02-11 19:45:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AuthForm  from './AuthForm.js';
+
+function App() {
+  return (
+    <div>
+     <AuthForm/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ AuthForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+export default function AuthForm() {
+    const [isLogin,setIsLogin] = useState(true);
+    return (
+    <div className='container'> 
+        <div className='form-container'>
+            <div className='form-toggle'>
+                <button className= {isLogin ? 'active':""} onClick={()=>setIsLogin(true) }>Login</button>
+                <button className= {!isLogin ? 'active':""} onClick={()=>setIsLogin(false) }>SignUp</button>
+            </div>
+            {isLogin?<>
+            <div className='form'>
+                <h2>Login Form</h2>
+                <input type='email' placeholder='Email'/>
+                <input type='password' placeholder='Password'/>
+                <a href= '#'>Forgot Password?</a>
+                <button>Login</button>
+                <p>New here? <a href='#' onClick ={()=> setIsLogin(false )}>SignUp now!</a></p>
+            </div>
+            </> :<>
+            <div className='form'>
+            <h2>Signup Form</h2>
+                <input type='email' placeholder='Email'/>
+                <input type='password' placeholder='Password'/>
+                <input type='password' placeholder='Confirm Password'/>
+                <button>Signup</button>
+            </div>
+            </>}
+        </div>
+    </div>
+)
+}
